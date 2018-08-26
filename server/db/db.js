9f83cf7445bc05095690206a42e0b400bccd6154
Jeanne Castillo <jeanne@cedrus.digital>
2018-08-26 19:43:41 -0400

    log response from geocoding API

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost:5432/weatherwear', {
+  process.env.DATABASE_URL || 'postgres://localhost:5433/weatherwear', {
     logging: false
   }
 )
